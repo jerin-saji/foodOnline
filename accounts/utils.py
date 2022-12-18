707b26af36d48dd19a6315d7654fb32bfe33b660
jerin <5.jerinsaji@gmail.com>
2022-12-18 15:32:35 +0530

    forgot password feature

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,4 +1,11 @@
-# from .models import User
+from .models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 
 
@@ -13,3 +20,30 @@ def detectUser(user):
     elif user.role==None and user.is_superadmin:
         redirectUrl='/admin'
         return redirectUrl
+
+def send_verification_email(request,user,mail_subject,email_template):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    current_site = get_current_site(request)
+    message = render_to_string(email_template,{
+        'user':user,
+        'domain':current_site,
+        'uid':urlsafe_base64_encode(force_bytes(user.pk)),# take user primary key encode it and send
+        'token':default_token_generator.make_token(user), # to generate token
+    })
+    to_email = user.email
+    mail = EmailMessage(mail_subject,message,from_email,to=[to_email])
+    mail.send()
+
+# def send_password_reset_email(request,user):
+#     from_email = settings.DEFAULT_FROM_EMAIL
+#     current_site = get_current_site(request)
+#     mail_subject = 'Reset your password'
+    # message = render_to_string('accounts/emails/reset_password_email.html',{
+#         'user':user,
+#         'domain':current_site,
+#         'uid':urlsafe_base64_encode(force_bytes(user.pk)),# take user primary key encode it and send
+#         'token':default_token_generator.make_token(user), # to generate token
+#     })
+#     to_email = user.email
+#     mail = EmailMessage(mail_subject,message,from_email,to=[to_email])
+#     mail.send()
